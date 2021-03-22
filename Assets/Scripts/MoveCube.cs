ec49c4f77d47a0d632d92381ebcd0a9f924a7170
Sam Gentry <gensa844@student.otago.ac.nz>
2021-03-23 01:17:30 +1300

    Added Assignment 1 to git

---
+++ Assets/Scripts/MoveCube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour
+{
+    public Vector3 pos;
+    public GameObject plane;
+    public GameObject plane1;
+
+    void Start()
+    {
+        plane1 = GameObject.Find("Plane");
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        pos = this.transform.position;
+        this.transform.position += new Vector3(Time.deltaTime*1, 0, 0);
+    }
+}
