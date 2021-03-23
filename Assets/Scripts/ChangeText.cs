fee97f8b086e12dbbe1fc35773c652f79a7853f0
Sam Gentry <gensa844@student.otago.ac.nz>
2021-03-23 14:16:56 +1300

    text object displays camera position

---
+++ Assets/Scripts/ChangeText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeText : MonoBehaviour
+{
+    public GameObject camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GameObject.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.GetComponent<TextMesh>().text = (camera.transform.position.ToString());
+    }
+}
