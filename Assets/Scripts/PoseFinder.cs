18403e822e2a73e1584607de661105adbd9a4f6a
Sam Gentry <gensa844@student.otago.ac.nz>
2021-03-29 22:10:58 +1300

    last commit for today

---
+++ Assets/Scripts/PoseFinder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoseFinder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject otherObject;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Pose poseThis = new Pose(this.gameObject.transform.position, this.gameObject.transform.rotation);
+        Pose poseThat = new Pose(otherObject.transform.position, otherObject.transform.rotation);
+        Pose poseRelative = this.gameObject.GetComponent<RelativePose>().ComputeRelativePose(poseThis, poseThat);
+        Debug.Log(poseRelative);
+    }
+}
---
+++ Assets/Scripts/RunTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunTest : MonoBehaviour
+{
+    private GameObject cameraObj;
+    private string camPosX;
+    private string camPosY;
+    private string camPosZ;
+    private string allCamPosCsv;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        camPosX = this.gameObject.transform.position.x.ToString();
+        camPosY = this.gameObject.transform.position.y.ToString();
+        camPosZ = this.gameObject.transform.position.z.ToString();
+        
+    }
+    
+    public void Test()
+    {
+        //Debug.Log("button clicked");
+        allCamPosCsv += (camPosX + "," + camPosY + "," + camPosZ + ",\n");
+        this.gameObject.GetComponent<RecordData>().WriteData("test.csv", allCamPosCsv);  
+    }
+}
