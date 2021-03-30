69118be88ffad6822c9fc07960112a1966d2b188
Sam Gentry <gensa844@student.otago.ac.nz>
2021-03-30 14:32:00 +1300

    trying to get run test script to work how we want it

--- Assets/Scripts/RunTest.cs
+++ Assets/Scripts/RunTest.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class RunTest : MonoBehaviour
 {
-    private GameObject cameraObj;
+    //private GameObject cameraObj;
     private string camPosX;
     private string camPosY;
     private string camPosZ;
     private string allCamPosCsv;
+    private bool testing = false;
+    //private var startTime = 0;
+    //private var timeTaken = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,16 +21,29 @@ public class RunTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        camPosX = this.gameObject.transform.position.x.ToString();
-        camPosY = this.gameObject.transform.position.y.ToString();
-        camPosZ = this.gameObject.transform.position.z.ToString();
-        
+        //camPosX = this.gameObject.transform.position.x.ToString();
+        //camPosY = this.gameObject.transform.position.y.ToString();
+        //camPosZ = this.gameObject.transform.position.z.ToString();
+        if (testing == true)
+        {
+            var startTime = Time.deltaTime;
+            var timeTaken = Time.deltaTime - startTime;
+            while (timeTaken <= 8)
+            {
+                //allCamPosCsv += (camPosX + "," + camPosY + "," + camPosZ + ",\n");
+                Debug.Log(timeTaken);
+                timeTaken = Time.deltaTime - startTime;
+            }
+            testing = false;
+        }
     }
     
     public void Test()
     {
         //Debug.Log("button clicked");
-        allCamPosCsv += (camPosX + "," + camPosY + "," + camPosZ + ",\n");
-        this.gameObject.GetComponent<RecordData>().WriteData("test.csv", allCamPosCsv);  
+        testing = true;
+        
+        //this.gameObject.GetComponent<RecordData>().WriteData("test.csv", allCamPosCsv);  
     }
+
 }
